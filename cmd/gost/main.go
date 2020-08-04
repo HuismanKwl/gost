0756bd93e8f6a47c702c9b1fbda4c022c8ce9199
huisman <kwl199302010@sina.com>
2020-08-04 14:54:36 +0800

    bug fix

--- cmd/gost/main.go
+++ cmd/gost/main.go
@@ -34,7 +34,7 @@ func init() {
 	flag.StringVar(&configureFile, "C", "", "configure file")
 	flag.BoolVar(&baseCfg.Debug, "D", false, "enable debug log")
 	flag.BoolVar(&printVersion, "V", false, "print version")
-	flag.BoolVar(&baseCfg.OutIP, "I", "", "specify out ip")
+	flag.StringVar(&baseCfg.OutIP, "I", "", "specify out ip")
 	if pprofEnabled {
 		flag.StringVar(&pprofAddr, "P", ":6060", "profiling HTTP server address")
 	}
